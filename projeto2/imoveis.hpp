06cabd7e9734850ebd3b6591a48ef3414aac3208
carlosvinicius01 <carloskic1@gmail.com>
2018-12-08 21:11:52 -0300

    A morte não é o oposto da vida, mas parte dela.

--- projeto2/imoveis.hpp
+++ projeto2/imoveis.hpp
@@ -6,6 +6,7 @@
 class Imovel
 {
   protected:
+    std::string tituloAnuncio;
     std::string rua;
     std::string bairro;
     std::string cep;
@@ -15,8 +16,9 @@ class Imovel
     char disponibilidade; // 'v' - vender / 'a' - alugar
 
   public:
-    Imovel() : rua(""), bairro(""), cep(""), cidade(""), numero(0), valor(0), disponibilidade(0) {}
+    Imovel() : tituloAnuncio(""), rua(""), bairro(""), cep(""), cidade(""), numero(0), valor(0), disponibilidade(0) {}
 
+    std::string getTituloAnuncio();
     std::string getRua();
     std::string getBairro();
     std::string getCep();
@@ -25,6 +27,7 @@ class Imovel
     double getValor();
     char getDisponibilidade();
 
+    void setTituloAnuncio(std::string t);
     void setRua(std::string r);
     void setBairro(std::string b);
     void setCep(std::string c);
@@ -32,11 +35,59 @@ class Imovel
     void setNumero(int num);
     void setValor(double val);
     void setDisponibilidade(char disp);
+
+    virtual void cadastrar() = 0;
+    virtual void exibir() = 0;
 };
 
 class Casa : public Imovel
 {
+  private:
+    int nPavimentos, nQuartos;
+    double areaTerreno, areaConstruida;
+
+  public:
+    Casa() : nPavimentos(0), nQuartos(0), areaTerreno(0), areaConstruida(0){};
+
+    int getNPavimentos();
+    int getNQuartos();
+    double getAreaTerreno();
+    double getAreaConstruida();
+
+    void setNPavimentos(int nP);
+    void setNQuartos(int nQ);
+    void setAreaTerreno(double aT);
+    void setAreaConstruida(double aC);
+
+    void cadastrar();
+    void exibir();
     
+};
+
+class Apartamento : public Imovel
+{
+  private:
+    int andar, nQuartos, nVagas;
+    double area;
+    char posicao;
+
+  public:
+    Apartamento() : andar(0), nQuartos(0), nVagas(0), area(0), posicao(0){};
+
+    int getAndar();
+    int getNQuartos();
+    int getNVagas();
+    double getArea();
+    char getPosicao();
+
+    void setAndar(int a);
+    void setNQuartos(int nQ);
+    void setNVagas(int nV);
+    void setArea(double a);
+    void setPosicao(char p);
+
+    void cadastrar();
+    void exibir();
 };
 
 #endif
--- projeto2/main.cpp
+++ projeto2/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <string>
 #include <list>
+#include <vector>
 #include "imoveis.hpp"
 
 int main()
 {
-    Casa casa;
+    std::vector<Imovel*> imovel; // lista dinamica de imoveis
     
-    
+    imovel.push_back(new Casa); //adicionando mais imoveis
+    imovel.push_back(new Apartamento);
+
+    imovel[0]->exibir(); //vai exibir uma casa zerada
+    imovel[1]->exibir(); // vai exibir um apartamento zerado
+
 }
