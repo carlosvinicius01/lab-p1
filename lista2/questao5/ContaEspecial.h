d594323e21638b214abb33bb0f6678576029f611
carlos <carloskic1@gmail.com>
2019-04-04 20:33:01 -0300

    a

---
+++ lista2/questao5/ContaEspecial.h
@@ -0,0 +1,13 @@
+#ifndef CONTAESPECIAL_H
+#define CONTAESPECIAL_H
+
+#include "Conta.h"
+
+class ContaEspecial : public Conta
+{
+  public:
+    ContaEspecial(std::string n, std::string c, double sm, double s);
+    void definirLimite();
+};
+
+#endif // CONTAESPECIAL_H
---
+++ lista2/questao5/SaldoNaoDisponivelException.h
@@ -0,0 +1,11 @@
+#ifndef SALDONAODISPONIVEL_H
+#define SALDONAODISPONIVEL_H
+
+#include <exception>
+
+class SaldoNaoDisponivelException : public std::exception
+{
+
+};
+
+#endif
